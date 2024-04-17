1a972d712f64bd4283d52178f14dca40d5134584
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-04-18 02:23:35 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/user_data', methods=['POST'])
+def user_data():
+    data = request.get_json()
+
+    print(data)
+    return jsonify({"received_data": data})
+
+if __name__ == '__main__':
+    app.run(debug=True)
